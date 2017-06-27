6ba07eff11b8141f4e831ff54f0e0d55e6b8751d
Sdonai <silentstrum@gmail.com>
2017-06-26 21:37:06 -0700

    Cities now take the name of the GameObject for saves & text

---
+++ Assets/CityEditorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class CityEditorScript : MonoBehaviour {
+
+	Text cityName;
+	void Awake (){
+		cityName = gameObject.GetComponentInChildren<Text> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Name is now: " + gameObject.name);
+		cityName.text = gameObject.name;
+	}
+}
--- Assets/Controllers/CityCtrl.cs
+++ Assets/Controllers/CityCtrl.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class CityCtrl : MonoBehaviour {
-	public int wealth = 0;
 	// Use this for initialization
 	public City city;
-	public string name;
 	void Start () {
 		Button btn = gameObject.GetComponent<Button> ();
 		btn.onClick.AddListener (displayCity);
 	}
+
 	public void addWealth(){
 		city.money += 1;
 	}
+
 	void displayCity (){
 		Debug.Log ("Display City has been Clicked");
 		//set shop to city
