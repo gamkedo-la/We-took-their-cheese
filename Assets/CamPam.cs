9cdf6001df7f86273cf2856c382b7cb2f7740653
Sdonai <silentstrum@gmail.com>
2017-07-02 12:14:20 -0700

    Overworld map changes

---
+++ Assets/CamPam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position += Time.deltaTime * (Input.GetAxis ("Horizontal") * Vector3.right + Input.GetAxis ("Vertical") * Vector3.up) * 200.0f;
+
+	}
+}
